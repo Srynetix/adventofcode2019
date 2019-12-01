b31e93c085dd8fe6a78d77ab3308cfbd1caafcba
Denis Bourge <denis.bourge@sharingcloud.com>
2019-12-01 14:15:15 +0100

    feat: ci

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,34 +2,34 @@
 
 fn part1(input_txt: &str) {
     let mut sum = 0;
-    
-    for line in input_txt.split("\n") {
+
+    for line in input_txt.split('\n') {
         let mass: f32 = line.parse().unwrap();
         let fuel = (mass / 3.0).floor() - 2.0;
         sum += fuel as u32;
     }
-    
+
     println!("[Part 1]");
     println!("{}", sum);
 }
 
 fn part2(input_txt: &str) {
     let mut sum = 0;
-    
-    for line in input_txt.split("\n") {
+
+    for line in input_txt.split('\n') {
         let mut mass: f32 = line.parse().unwrap();
 
         loop {
             let fuel = (mass / 3.0).floor() - 2.0;
             if fuel < 0.0 {
-                break
+                break;
             }
-            
+
             sum += fuel as i32;
             mass = fuel;
         }
     }
-    
+
     println!("[Part 2]");
     println!("{}", sum);
 }
