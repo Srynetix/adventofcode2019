4267e2819af51bdc54a54993559a324ee287defe
Denis Bourge <denis.bourge@sharingcloud.com>
2019-12-10 19:18:53 +0100

    feat: day10 part 1 OK

--- aocutils/src/math.rs
+++ aocutils/src/math.rs
@@ -8,6 +8,52 @@ pub fn float_eq_eps(a: f32, b: f32, eps: f32) -> bool {
     (a - b).abs() < eps
 }
 
+/// Compute bresenham line between two points
+/// Adapted from https://www.codeproject.com/Articles/15604/Ray-casting-in-a-2D-tile-based-environment
+pub fn bresenham_line(x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<(i32, i32)> {
+    let mut x1 = x1;
+    let mut x2 = x2;
+    let mut y1 = y1;
+    let mut y2 = y2;
+
+    let mut result = vec![];
+    let absx = (x2 - x1).abs();
+    let absy = (y2 - y1).abs();
+
+    let steep = absy > absx;
+    if steep {
+        std::mem::swap(&mut x1, &mut y1);
+        std::mem::swap(&mut x2, &mut y2);
+    }
+
+    if x1 > x2 {
+        std::mem::swap(&mut x1, &mut x2);
+        std::mem::swap(&mut y1, &mut y2);
+    }
+
+    let deltax = x2 - x1;
+    let deltay = (y2 - y1).abs();
+    let mut error = 0;
+    let mut y = y1;
+    let ystep = if y1 < y2 { 1 } else { -1 };
+
+    for x in x1..=x2 {
+        if steep {
+            result.push((y, x));
+        } else {
+            result.push((x, y));
+        }
+
+        error += deltay;
+        if 2 * error >= deltax {
+            y += ystep;
+            error -= deltax;
+        }
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -16,5 +62,14 @@ mod tests {
     fn test_eq() {
         assert!(float_eq(1.0, 1.0));
         assert!(float_eq(1.000000, 1.000001));
+    }
+
+    #[test]
+    fn test_bresenham() {
+        let ((x1, y1), (x2, y2)) = ((0, 0), (4, 5));
+        assert_eq!(
+            bresenham_line(x1, y1, x2, y2),
+            vec![(0, 0), (1, 1), (2, 2), (2, 3), (3, 4), (4, 5)]
+        );
     }
 }
--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,3 +1,260 @@
+use itertools::Itertools;
+
+#[derive(Debug, Clone)]
+pub struct AsteroidMap {
+    data: Vec<char>,
+    asteroid_locations: Vec<(usize, usize)>,
+    width: usize,
+    height: usize,
+}
+
+impl AsteroidMap {
+    pub fn from_input(input_txt: &str) -> Self {
+        let lines: Vec<_> = input_txt.split('\n').collect();
+        let width = lines[0].chars().count();
+        let height = lines.len();
+        let mut asteroid_locations = vec![];
+        let mut data = vec![];
+
+        for (y, line) in lines.iter().enumerate() {
+            for (x, c) in line.chars().enumerate() {
+                if c == '#' {
+                    asteroid_locations.push((x, y));
+                }
+
+                data.push(c);
+            }
+        }
+
+        Self {
+            data,
+            width,
+            height,
+            asteroid_locations,
+        }
+    }
+
+    pub fn get_char(&self, x: usize, y: usize) -> char {
+        if x >= self.width || y >= self.height {
+            ' '
+        } else {
+            self.data[x + y * self.width]
+        }
+    }
+
+    /// Scan asteroids at point
+    pub fn scan_point(&self, x: usize, y: usize) -> usize {
+        if self.get_char(x, y) == '.' {
+            return 0;
+        }
+
+        self.asteroid_locations
+            .iter()
+            .filter(|(ax, ay)| *ax != x || *ay != y)
+            .map(|(ax, ay)| {
+                let x = x as f32;
+                let y = y as f32;
+                let ax = *ax as f32;
+                let ay = *ay as f32;
+                (-(x - ax).atan2(y - ay) * 1000.0) as i32
+            })
+            .unique()
+            .count()
+    }
+
+    pub fn better_position(&self) -> ((usize, usize), usize) {
+        let mut better_pos = (0, 0);
+        let mut better_count = 0;
+
+        for (ax, ay) in &self.asteroid_locations {
+            let count = self.scan_point(*ax, *ay);
+            if count > better_count {
+                better_count = count;
+                better_pos = (*ax, *ay);
+            }
+        }
+
+        (better_pos, better_count)
+    }
+
+    pub fn dump_scan(&self) -> String {
+        let mut output = String::new();
+
+        for y in 0..self.height {
+            for x in 0..self.width {
+                if self.get_char(x, y) == '.' {
+                    output.push('.');
+                } else {
+                    output.push_str(&self.scan_point(x, y).to_string());
+                }
+            }
+
+            if y != self.height - 1 {
+                output.push('\n');
+            }
+        }
+
+        output
+    }
+
+    pub fn dump(&self) -> String {
+        let mut output = String::new();
+
+        for y in 0..self.height {
+            for x in 0..self.width {
+                output.push(self.data[x + y * self.width]);
+            }
+
+            if y != self.height - 1 {
+                output.push('\n');
+            }
+        }
+
+        output
+    }
+}
+
+fn part1(input_txt: &str) -> usize {
+    let map = AsteroidMap::from_input(input_txt);
+    let (_, count) = map.better_position();
+    count
+}
+
+fn part2(_input_txt: &str) -> usize {
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_txt = include_str!("../input.txt");
+
+    println!("[Part 1]");
+    let r = part1(&input_txt);
+    println!("Result: {}", r);
+
+    println!("[Part 2]");
+    let r = part2(&input_txt);
+    println!("Result: {}", r);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_small() {
+        let map = ".#..#\n\
+                   .....\n\
+                   #####\n\
+                   ....#\n\
+                   ...##";
+
+        let ast_map = AsteroidMap::from_input(map);
+        assert_eq!(ast_map.dump(), map);
+
+        assert_eq!(ast_map.scan_point(0, 0), 0);
+        assert_eq!(ast_map.scan_point(1, 0), 7);
+        assert_eq!(ast_map.scan_point(4, 2), 5);
+
+        assert_eq!(
+            ast_map.dump_scan(),
+            ".7..7\n\
+             .....\n\
+             67775\n\
+             ....7\n\
+             ...87"
+        );
+
+        assert_eq!(ast_map.better_position(), ((3, 4), 8));
+    }
+
+    #[test]
+    fn test_medium() {
+        assert_eq!(
+            AsteroidMap::from_input(
+                "......#.#.\n\
+                 #..#.#....\n\
+                 ..#######.\n\
+                 .#.#.###..\n\
+                 .#..#.....\n\
+                 ..#....#.#\n\
+                 #..#....#.\n\
+                 .##.#..###\n\
+                 ##...#..#.\n\
+                 .#....####"
+            )
+            .better_position(),
+            ((5, 8), 33)
+        );
+
+        assert_eq!(
+            AsteroidMap::from_input(
+                "#.#...#.#.\n\
+                 .###....#.\n\
+                 .#....#...\n\
+                 ##.#.#.#.#\n\
+                 ....#.#.#.\n\
+                 .##..###.#\n\
+                 ..#...##..\n\
+                 ..##....##\n\
+                 ......#...\n\
+                 .####.###."
+            )
+            .better_position(),
+            ((1, 2), 35)
+        );
+
+        assert_eq!(
+            AsteroidMap::from_input(
+                ".#..#..###\n\
+                 ####.###.#\n\
+                 ....###.#.\n\
+                 ..###.##.#\n\
+                 ##.##.#.#.\n\
+                 ....###..#\n\
+                 ..#.#..#.#\n\
+                 #..#.#.###\n\
+                 .##...##.#\n\
+                 .....#.#.."
+            )
+            .better_position(),
+            ((6, 3), 41)
+        );
+    }
+
+    #[test]
+    fn test_big() {
+        assert_eq!(
+            AsteroidMap::from_input(
+                ".#..##.###...#######\n\
+                 ##.############..##.\n\
+                 .#.######.########.#\n\
+                 .###.#######.####.#.\n\
+                 #####.##.#.##.###.##\n\
+                 ..#####..#.#########\n\
+                 ####################\n\
+                 #.####....###.#.#.##\n\
+                 ##.#################\n\
+                 #####.##.###..####..\n\
+                 ..######..##.#######\n\
+                 ####.##.####...##..#\n\
+                 .#####..#.######.###\n\
+                 ##...#.##########...\n\
+                 #.##########.#######\n\
+                 .####.#.###.###.#.##\n\
+                 ....##.##.###..#####\n\
+                 .#.#.###########.###\n\
+                 #.#.#.#####.####.###\n\
+                 ###.##.####.##.#..##"
+            )
+            .better_position(),
+            ((11, 13), 210)
+        );
+    }
+
+    #[test]
+    fn test_results() {
+        let input_txt = include_str!("../input.txt");
+        assert_eq!(part1(&input_txt), 329);
+        // assert_eq!(part2(&input_txt), 76_642);
+    }
 }
