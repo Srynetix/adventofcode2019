ca6628a1f619c7d85df921ce70cbc91eeaa1f82f
Denis Bourge <denis.bourge@sharingcloud.com>
2019-12-11 10:52:52 +0100

    feat: day 11 part 2 OK

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,6 +1,6 @@
-use common::interpreter::{ExecutionState, Interpreter};
-
 use std::collections::HashMap;
+
+use common::interpreter::{ExecutionState, Interpreter};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Direction {
@@ -11,29 +11,29 @@ pub enum Direction {
 }
 
 impl Direction {
-    pub fn rotate_right(&self) -> Self {
+    pub fn rotate_right(self) -> Self {
         match self {
             Self::Up => Self::Right,
             Self::Right => Self::Down,
             Self::Down => Self::Left,
-            Self::Left => Self::Up
+            Self::Left => Self::Up,
         }
     }
 
-    pub fn rotate_left(&self) -> Self {
+    pub fn rotate_left(self) -> Self {
         match self {
             Self::Up => Self::Left,
             Self::Left => Self::Down,
             Self::Down => Self::Right,
-            Self::Right => Self::Up
+            Self::Right => Self::Up,
         }
     }
 
-    pub fn with_rotation_code(&self, code: i64) -> Self {
+    pub fn with_rotation_code(self, code: i64) -> Self {
         match code {
             0 => self.rotate_left(),
             1 => self.rotate_right(),
-            _ => panic!("wrong code")
+            _ => panic!("wrong code"),
         }
     }
 }
@@ -45,10 +45,10 @@ pub enum Color {
 }
 
 impl Color {
-    pub fn to_code(&self) -> i64 {
+    pub fn to_code(self) -> i64 {
         match self {
             Self::Black => 0,
-            Self::White => 1
+            Self::White => 1,
         }
     }
 
@@ -56,16 +56,16 @@ impl Color {
         match code {
             0 => Self::Black,
             1 => Self::White,
-            _ => panic!("wrong code")
+            _ => panic!("wrong code"),
         }
     }
 }
 
 impl Direction {
-    pub fn to_offset(&self) -> (i32, i32) {
+    pub fn to_offset(self) -> (i32, i32) {
         match self {
-            Self::Up => (0, 1),
-            Self::Down => (0, -1),
+            Self::Up => (0, -1),
+            Self::Down => (0, 1),
             Self::Left => (-1, 0),
             Self::Right => (1, 0),
         }
@@ -96,36 +96,89 @@ impl DrawSim {
         self.tiles.insert(self.robot_position, color);
     }
 
-    pub fn get_color_position(&self) -> Color {
-        self.tiles.get(&self.robot_position).copied().unwrap_or(Color::Black)
+    pub fn get_color_at_robot(&self) -> Color {
+        self.get_color_at_position(self.robot_position)
     }
 
-    pub fn move_robot(&mut self, direction: &Direction) {
+    pub fn get_color_at_position(&self, pos: (i32, i32)) -> Color {
+        self.tiles.get(&pos).copied().unwrap_or(Color::Black)
+    }
+
+    pub fn move_robot(&mut self, direction: Direction) {
         let (ox, oy) = direction.to_offset();
         let (rx, ry) = self.robot_position;
         self.robot_position = (rx + ox, ry + oy);
     }
 
-    pub fn run(&mut self) {
+    pub fn run(&mut self, base_color: Color) {
         let mut direction = Direction::Up;
-        let mut color = Color::Black;
+        let mut color = base_color;
 
         loop {
             let (new_color, new_direction, state) = self.step(color, direction);
-            match state {
-                ExecutionState::Exit => break,
-                _ => ()
+            if let ExecutionState::Exit = state {
+                break;
             }
 
             // Paint
             self.paint_position(new_color);
-            self.move_robot(&new_direction);
+            self.move_robot(new_direction);
             direction = new_direction;
-            color = self.get_color_position();
+            color = self.get_color_at_robot();
         }
     }
 
-    pub fn step(&mut self, color: Color, direction: Direction) -> (Color, Direction, ExecutionState) {
+    pub fn get_rect(&self) -> (i32, i32, i32, i32) {
+        let mut x_min = i32::max_value();
+        let mut x_max = i32::min_value();
+        let mut y_min = i32::max_value();
+        let mut y_max = i32::min_value();
+
+        for (x, y) in self.tiles.keys().copied() {
+            if x < x_min {
+                x_min = x;
+            }
+            if x > x_max {
+                x_max = x;
+            }
+            if y < y_min {
+                y_min = y;
+            }
+            if y > y_max {
+                y_max = y;
+            }
+        }
+
+        (x_min, y_min, x_max - x_min, y_max - y_min + 1)
+    }
+
+    pub fn draw(&self) -> String {
+        let mut string = String::new();
+        let (ox, oy, w, h) = self.get_rect();
+
+        for y in oy..oy + h {
+            for x in ox..ox + w {
+                let color = self.get_color_at_position((x, y));
+                if color == Color::Black {
+                    string.push_str("  ");
+                } else {
+                    string.push_str("██");
+                }
+            }
+
+            if y != oy + h - 1 {
+                string.push('\n');
+            }
+        }
+
+        string
+    }
+
+    pub fn step(
+        &mut self,
+        color: Color,
+        direction: Direction,
+    ) -> (Color, Direction, ExecutionState) {
         self.interpreter.push_input(color.to_code());
 
         loop {
@@ -149,12 +202,14 @@ impl DrawSim {
 
 fn part1(input_txt: &str) -> usize {
     let mut sim = DrawSim::new(input_txt);
-    sim.run();
+    sim.run(Color::Black);
     sim.tiles.keys().count()
 }
 
-fn part2(input_txt: &str) -> usize {
-    0
+fn part2(input_txt: &str) -> String {
+    let mut sim = DrawSim::new(input_txt);
+    sim.run(Color::White);
+    sim.draw()
 }
 
 fn main() {
@@ -166,7 +221,7 @@ fn main() {
 
     println!("[Part 2]");
     let r = part2(&input_txt);
-    println!("Result: {}", r);
+    println!("Result:\n{}", r);
 }
 
 #[cfg(test)]
@@ -183,7 +238,8 @@ mod tests {
     #[test]
     fn test_results() {
         let input_txt = include_str!("../input.txt");
+        let part2_result = include_str!("../part2_result.txt");
         assert_eq!(part1(&input_txt), 2_088);
-        // assert_eq!(part2(&input_txt), 0);
+        assert_eq!(part2(&input_txt), part2_result);
     }
 }
